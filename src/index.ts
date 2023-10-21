2861058e399649219280dea7d2a1cc3d18f2945e
Aylex Nullzero <p4rlx-news@pm.me>
2023-10-22 03:11:00 +1030

    initial files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-console.log(
-  "This is a starter kit for this amazing project. With 💓 By Indian Coders"
-);
+import express from "express";
+
+const app = express();
+
+// GET, POST, DELETE, PUT
+
+
+
+app.get("/hello", (req, res, next) => {
+  return res.send("Hello");
+  
+});
+
+app.listen(5001, () => console.log("Server is open on port 5001"));
+   
+ 
